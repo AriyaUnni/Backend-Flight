2d77c9ac75b3c303256aea0cf35cb25ac95ada24
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-19 18:49:00 +0530

    last commit

--- src/main/java/com/nestdigital/flightbackendApplication/controller/FlightController.java
+++ src/main/java/com/nestdigital/flightbackendApplication/controller/FlightController.java
@@ -1,13 +1,29 @@
 package com.nestdigital.flightbackendApplication.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.nestdigital.flightbackendApplication.dao.FlightDao;
+import com.nestdigital.flightbackendApplication.model.FlightModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class FlightController {
 
-    @PostMapping("/addflight")
-    public String addFlight(){
-        return "test";
+    @Autowired
+    private FlightDao dao;
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/addflight",consumes = "application/json",produces = "application/json")
+    public String addFlight(@RequestBody FlightModel flight){
+        System.out.println(flight.toString());
+        dao.save(flight);
+        return "{status:'success'}";
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/viewflight")
+    public List<FlightModel> viewFlights(){
+        return (List<FlightModel>) dao.findAll();
     }
 }
